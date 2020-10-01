8ba923e75eac076ccfa0d6dc462597519697077c
gylych <mammedov.207@gmail.com>
2020-10-01 01:29:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  const arr = []
+
+  for (let i = 0; i < musicians.length; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return arr
+}
+
+function johnLennonFacts(facts){
+  let newArr = []
+  let i=0;
+while(i<facts.length){
+  newArr.push(`${facts[i]}!!!`);
+  i++;
+}
+return newArr
+}
+
+function iLoveTheBeatles(n){
+
+  const arr = [];
+  do{
+    arr.push(`I love the Beatles!`)
+    n++
+  }while(n < 15);
+  return arr;
+}
